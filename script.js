7f62cfd8070632e1cee82f8a929f587483be7f05
Ross Clay <131317443+rossclay@users.noreply.github.com>
2024-02-07 20:36:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+// When the user inputs a case insensitive string choosing either rock, paper, or scissors
+// The computer should randomly choose either rock, paper, or scissors
+// The user's choice should be compared against the computer's
+// A winner will be determined based on the following rules
+// Rock beats Scissors
+// Paper beats Rock
+// Scissors beats paper
+// If the choices match, there is a tie
+// Inform the user of the result
+// If the result is a tie, there must be rematch
+// NOTE TO SELF: consider adjusting all variables inside the "playRound" function from "game" to "round"
+
+// Randomly generate computer's choice
+function getComputerChoice() {
+    let computerChoice
+    let randomnNumber = Math.floor(Math.random() * 3) + 1;
+    if (randomnNumber === 1) {
+        computerChoice = 'Rock'
+    }
+    else if (randomnNumber === 2) {
+        computerChoice = 'Paper'
+    }
+    else computerChoice = 'Scissors'
+    // console.log(computerChoice)
+    return computerChoice
+};
+let computerSelection = getComputerChoice();
+
+// capitalize function to allow for case insensitive input
+function capitalize(string) {
+    let firstLetter = string.slice(0, 1);
+    firstLetter = firstLetter.toUpperCase()
+    let restOfString = string.slice(1);
+    restOfString = restOfString.toLowerCase()
+    return firstLetter + restOfString;
+}
+
+// player inputs case insensitive selection
+function getPlayerChoice() {
+    let playerChoice = capitalize(prompt('Please choose either rock, paper, or scissors.'))
+    return playerChoice
+}
+let playerSelection = 'rock';
+let gameStatus
+
+// determine a winner
+function playRound(playerSelection, computerSelection) {
+    let gameMessage = `You ${gameStatus}! You chose ${playerSelection}, but the computer chose ${computerSelection}.`
+    // losing scenarios
+    if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
+        gameStatus = 'Lose'
+        console.log(gameMessage)
+        return gameStatus
+    }
+    else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
+        gameStatus = 'Lose'
+        console.log(gameMessage)
+        return gameStatus
+    }
+    else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
+        gameStatus = 'Lose'
+        console.log(gameMessage)
+        return gameStatus
+    }
+    // winning scenarios
+    else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        gameStatus = 'Win'
+        console.log(gameMessage)
+        return gameStatus
+    }
+    else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        gameStatus = 'Win'
+        console.log(gameMessage)
+        return gameStatus
+    }
+    else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        gameStatus = 'Win'
+        console.log(gameMessage)
+        return gameStatus
+    }
+    // tie
+    else if (computerSelection === playerSelection) {
+        gamestatus = 'Tied'
+        gameMessage = gameMessage.concat(' Try again.')
+        console.log(gameMessage)
+    } return gameStatus
+}
+
+// play 5 rounds
+function playGame() {
+    let i
+    let wins = 0
+    let losses = 0
+    let ties = 0
+    while (i < 5) {
+        playRound()
+        // keep score
+        if (gameStatus === 'Win') {
+            wins++
+        }
+        else if (gameStatus === 'Lose') {
+            losses++
+        } else if (gameStatus === 'Tied') {
+            ties++
+        }
+        // update score with a message
+        i++
+    }
+}
